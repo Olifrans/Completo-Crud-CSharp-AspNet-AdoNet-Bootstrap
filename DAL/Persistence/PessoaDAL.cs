e56bf1e56829af4395b7f1661ecd797cf6bf21d2
Francisco Oliveira <fransoliveira@gmail.com>
2021-03-07 20:07:51 -0300

    Crud completo com bootstrap e Jquery

--- DAL/Persistence/PessoaDAL.cs
+++ DAL/Persistence/PessoaDAL.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient; //ADO NET --> SQL SERVER
+using DAL.Model;
 
 namespace DAL.Persistence
 {
     class PessoaDAL
     {
+
     }
 }
 
